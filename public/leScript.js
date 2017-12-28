ab315d99513c077902854d0f49b61442a8467c39
swapsCAPS <verstegen.daan@gmail.com>
2017-12-28 14:12:40 +0100

    gedicht tijd Waaaah

--- public/leScript.js
+++ public/leScript.js
@@ -1,4 +1,38 @@
+var sentences = [
+  "Dit is een zin van het gedicht en het is ook nog eens een hele lange zin en nog veel langer omg gaat maar door waajooooo",
+  "Zinnetje deux",
+  "en Drie"
+]
+
+var index = 0
+
+function prev() {
+  console.log("prev")
+  if (index > 0) index--
+  setHtml()
+}
+
+function next() {
+  console.log("next")
+  index++
+  setHtml()
+}
+
+function setHtml() {
+  var sentence = sentences[index]
+  if (!sentence) {
+    sentence = "Klaar met dit gezeik wil je je cadeautje?"
+  }
+  var newText = $("<p>", { class: "le-text text-center" }).attr("id", "text").html(sentence)
+  $("#text").remove()
+  // setTimeout(function() {
+    $("#text-wrapper").append(newText)
+  // }, 100)
+}
+
 $(document).ready(function() {
-  console.log('je moededr')
+  $("#text").html(sentences[index])
+  $("#prev").click(prev)
+  $("#next").click(next)
 })
 
